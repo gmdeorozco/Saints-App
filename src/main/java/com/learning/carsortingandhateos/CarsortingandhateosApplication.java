02e337e6316ac42510ab73925d10b65f3b1c3ad7
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2022-11-21 07:23:58 +0000

    first commit

---
+++ src/main/java/com/learning/carsortingandhateos/CarsortingandhateosApplication.java
@@ -0,0 +1,13 @@
+package com.learning.carsortingandhateos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsortingandhateosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsortingandhateosApplication.class, args);
+	}
+
+}
